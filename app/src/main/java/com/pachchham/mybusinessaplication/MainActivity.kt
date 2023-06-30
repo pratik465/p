72f2474e557cf40259fe08c16149a730397b057e
pratik <pratikgangadiya465@gmail.com>
2023-06-30 10:31:04 +0530

    Initial commit

---
+++ app/src/main/java/com/pachchham/mybusinessaplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pachchham.mybusinessaplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pachchham.mybusinessaplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding
+    }
+}
